39c015df9f0db86b99240210ddb2e175a94626b2
capel82 <sandoralai@gmail.com>
2020-05-31 16:35:16 +0000

    created basket app with urls, views and basket.html.

--- abs_baking/urls.py
+++ abs_baking/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('courses', include('courses.urls')),
+    path('courses/', include('courses.urls')),
+    path('basket/', include('basket.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

ca23e95f4f01a94588722cfe4a075a558e596d87
capel82 <sandoralai@gmail.com>
2020-05-31 15:35:35 +0000

    created individual course.html fetching data from database

--- courses/urls.py
+++ courses/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_courses, name="courses"),
-   
+    path('<int:course_id>', views.course, name='course'),
+
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Course
 
 def all_courses(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all courses, including sorting and search queries """
 
     courses = Course.objects.all()
 
@@ -12,3 +12,11 @@ def all_courses(request):
 
     return render(request, 'courses/courses.html', context)
 
+def course(request, course_id):
+
+    course = get_object_or_404(Course, pk=course_id)
+
+    content = {
+        'course' : course
+    }
+    return render(request, 'courses/individual_course.html', content)

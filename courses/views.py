2c017689d5e36384137d0758be81a69189c895ce
capel82 <sandoralai@gmail.com>
2020-05-31 16:15:50 +0000

    changed heading in courses.html and added search by category function to main-nav.html

--- courses/views.py
+++ courses/views.py
@@ -1,15 +1,25 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Course
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Course, Category
 
 def all_courses(request):
     """ A view to show all courses, including sorting and search queries """
 
     courses = Course.objects.all()
+    query = None
+    category = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            courses = courses.filter(category__title__in=categories)
+            categories = Category.objects.filter(title__in=categories)
 
     context = {
         'courses': courses,
+        'current_categories': category,
     }
-
     return render(request, 'courses/courses.html', context)
 
 def course(request, course_id):

8b5f72a30713e385765612340a56ee581446cb87
capel82 <sandoralai@gmail.com>
2020-06-01 11:37:06 +0000

    updated contexts.py for count and price to show in the basket

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,11 +1,24 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from courses.models import Course
 
 def basket_contents(request):
 
     basket_items = []
     total = 0
     item_count = 0
+    basket = request.session.get('basket', {})
     
+    for course_id, quantity in basket.items():
+        course = get_object_or_404(Course, pk=course_id)
+        total += quantity * course.price
+        item_count += quantity
+        basket_items.append({
+            'course_id': course_id,
+            'quantity': quantity,
+            'course': course,
+        })
+
     grand_total = total
     
     context = {

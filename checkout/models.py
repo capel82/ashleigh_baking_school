8a30287e028bdeb8cd17f44f71a0dad55251fc6b
capel82 <sandoralai@gmail.com>
2020-06-09 11:49:04 +0000

    changed country in checkout form to placeholder using django-countries

--- checkout/models.py
+++ checkout/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+from django_countries.fields import CountryField
 
 from courses.models import Course
 
@@ -15,7 +16,7 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length = 50, null=False, blank=True)
     town_or_city = models.CharField(max_length = 50, null=False, blank=False)
     county = models.CharField(max_length = 50, null=False, blank=False)
-    country = models.CharField(max_length = 50, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length = 20, null=False, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     grand_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)

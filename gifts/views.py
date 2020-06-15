071b37646332736a271f8e630d9b4dc217ac1517
capel82 <sandoralai@gmail.com>
2020-06-15 11:51:45 +0000

    created urls.py and views.py for gifts app

--- gifts/views.py
+++ gifts/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from .models import Gift, Category
 # Create your views here.
+def all_gifts(request):
+
+    return render(request,'gifts/gifts.html')

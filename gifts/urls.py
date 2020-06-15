8f441273e9d7a6b67ed62509f279c1818aed7800
capel82 <sandoralai@gmail.com>
2020-06-15 14:52:28 +0000

    created single gift.html

--- gifts/urls.py
+++ gifts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_gifts, name="gifts"),
+     path('<int:gift_id>', views.gift, name='gift'),
 
 ]
--- gifts/views.py
+++ gifts/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Gift, Category
 # Create your views here.
 def all_gifts(request):
+    gifts = Gift.objects.all()
 
-    return render(request,'gifts/gifts.html')
+    context = {
+        'gifts': gifts,
+    }
+
+    return render(request,'gifts/gifts.html', context)
+
+def gift(request, gift_id):
+
+    gift = get_object_or_404(Gift, pk=gift_id)
+
+    context = {
+        'gift': gift
+    }
+    return render(request, 'gifts/single_gift.html', context)

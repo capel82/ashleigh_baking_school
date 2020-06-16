107b9ba4a4bd5177b550654377a2839a26ecb577
capel82 <sandoralai@gmail.com>
2020-06-16 10:19:05 +0000

    make main nav bootrap hoverable dropdown menu and filter category for gifts

--- gifts/views.py
+++ gifts/views.py
@@ -3,9 +3,18 @@ from .models import Gift, Category
 # Create your views here.
 def all_gifts(request):
     gifts = Gift.objects.all()
+    query = None
+    category = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            gifts = gifts.filter(category__title__in=categories)
+            categories = Category.objects.filter(title__in=categories)
 
     context = {
         'gifts': gifts,
+        'current_categories': category,
     }
 
     return render(request,'gifts/gifts.html', context)

2a57563e9e3da647d43163116688292077b753c4
capel82 <sandoralai@gmail.com>
2020-06-22 20:39:56 +0000

    customised admin area with css and displaying fields in courses

--- courses/admin.py
+++ courses/admin.py
@@ -5,6 +5,13 @@ from .models import Course, Category, SubCategory
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'category','day','course_datetime', 'is_published', 'is_next_event')
+    list_display_links = ('id', 'title')
+    list_filter = ('subcategory',)
+    list_editable = ('is_published', 'is_next_event')
+    list_per_page = 15
+    search_fields = ('title', 'description1','description2', 'learn_list1', 
+                    'learn_list2','learn_list3','completion_list1',
+                    'completion_list2','course_datetime', 'day')
 
     ordering = ('course_datetime',)
 

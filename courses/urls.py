01ed22d2ca3d4bc978c3fc85208234dec460bab0
capel82 <sandoralai@gmail.com>
2020-06-23 08:54:06 +0000

    travis file

--- courses/urls.py
+++ courses/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_courses, name="courses"),
-    path('<int:course_id>', views.course, name='course'),
+    path('<int:course_id>/', views.course, name='course'),
 
 ]
